e14b3a3b7f3391f79833f84d5746e37bb81c2246
Alexey Khomych <alexeyfurious@gmail.com>
2016-09-20 16:06:11 +0300

    added AKIView

---
+++ iOSProject/Source/UI/AKIUsersViewController/Views/AKIView/AKIView.h
@@ -0,0 +1,13 @@
+//
+//  AKIView.h
+//  iOSProject
+//
+//  Created by Alexey Khomych on 20.09.16.
+//  Copyright © 2016 Alexey Khomych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKIView : UIView
+
+@end
---
+++ iOSProject/Source/UI/AKIUsersViewController/Views/AKIView/AKIView.m
@@ -0,0 +1,22 @@
+//
+//  AKIView.m
+//  iOSProject
+//
+//  Created by Alexey Khomych on 20.09.16.
+//  Copyright © 2016 Alexey Khomych. All rights reserved.
+//
+
+#import "AKIView.h"
+
+#import "AKILoadingView.h"
+
+@implementation AKIView
+
+#pragma mark -
+#pragma mark Initializations and deallocations
+
+- (instancetype)init {
+    return [super init];
+}
+
+@end
