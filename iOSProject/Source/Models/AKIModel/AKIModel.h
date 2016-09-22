d8bd2a2cc32ea8273d180816d41be51f3038469e
Alexey Khomych <alexeyfurious@gmail.com>
2016-09-22 15:14:20 +0300

    added AKIModel

---
+++ iOSProject/Source/Models/AKIModel/AKIModel.h
@@ -0,0 +1,33 @@
+//
+//  AKIModel.h
+//  iOSProject
+//
+//  Created by Alexey Khomych on 21.09.16.
+//  Copyright © 2016 Alexey Khomych. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "AKIObservableObject.h"
+
+typedef NS_ENUM(NSUInteger, AKIModelState) {
+    AKIModelUpdated,
+    AKIModelDidLoad,
+    AKIModelWillLoad,
+    AKIModelFailedLoading
+};
+
+@protocol AKIObservableModel <NSObject>
+
+- (void)modelDidUpdated;
+- (void)modelWillLoad;
+- (void)modelDidLoad;
+- (void)modelFailedLoading;
+
+@end
+
+@interface AKIModel : AKIObservableObject
+
+- (void)load;
+
+@end
---
+++ iOSProject/Source/Models/AKIModel/AKIModel.m
@@ -0,0 +1,54 @@
+//
+//  AKIModel.m
+//  iOSProject
+//
+//  Created by Alexey Khomych on 21.09.16.
+//  Copyright © 2016 Alexey Khomych. All rights reserved.
+//
+
+#import "AKIModel.h"
+
+@implementation AKIModel
+
+#pragma mark -
+#pragma mark Public
+
+- (void)load {
+    @synchronized (self) {
+        if (AKIModelWillLoad == self.state) {
+            return;
+        }
+        
+        if (AKIModelDidLoad == self.state) {
+            [self notifyOfState:AKIModelDidLoad];
+            
+            return;
+        }
+        
+        self.state = AKIModelDidLoad;
+    }
+}
+
+#pragma mark -
+#pragma mark AKIObservableObject
+
+- (SEL)selectorForState:(NSUInteger)state {
+    switch (state) {
+        case AKIModelUpdated:
+            return @selector(modelDidUpdated:);
+            
+        case AKIModelFailedLoading:
+            return @selector(modelFailedLoading:);
+            
+        case AKIModelDidLoad:
+            return @selector(modelDidLoad:);
+            
+        case AKIModelWillLoad:
+            return @selector(modelWillLoad:);
+            
+        default:
+            return nil;
+    }
+}
+
+@end
